64c44b6b8d0cdc1d80ed7bd726dd8f3fe376191e
sparkhead95 <christianosholland@gmail.com>
2018-08-21 16:38:02 +0100

    Finished api call. url seems to be unreachable, but im able to ping the hostname

--- app/src/main/java/com/example/chris/myfirstapp/otherACTIVITY.java
+++ app/src/main/java/com/example/chris/myfirstapp/otherACTIVITY.java
@@ -1,5 +1,6 @@
 package com.example.chris.myfirstapp;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -9,18 +10,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class otherACTIVITY extends AppCompatActivity implements View.OnClickListener {
 
     TextView textView;
     Button showLocationBtn;
-
-    // Instantiate the RequestQueue.
-    RequestQueue queue = Volley.newRequestQueue(this);
-    String url ="http://www.google.com";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +42,29 @@ public class otherACTIVITY extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.showLocationBtn:
-                Log.d("tag", "Someone pushed the damn location button goddamnit");
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(this);
+                String url ="https://api.nasa.gov/planetary/apod?api_key=FmqsvLjKYhuEcXxTIdwrZwcYVFufNhGNFtL45m1N";
+                JsonObjectRequest objectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                textView.setText(response.toString());
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("Rest Response", error.toString());
+                            }
+                        }
+                );
 
+                queue.add(objectRequest);
 
-
-                textView.setText("Oi! You pushed my button!");
                 break;
         }
     }
